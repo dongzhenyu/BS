5fefd4d0b59eb33931d314e73294f45d8d97dc85
dzy <dzy@163.com>
2015-09-04 13:58:49 +0800

    初始化项目

---
+++ BS/BS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BS
+//
+//  Created by dzy on 15/9/4.
+//  Copyright (c) 2015年 董震宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BS/BS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BS
+//
+//  Created by dzy on 15/9/4.
+//  Copyright (c) 2015年 董震宇. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
