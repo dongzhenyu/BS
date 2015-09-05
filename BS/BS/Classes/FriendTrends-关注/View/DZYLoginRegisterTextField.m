dfe6006f2067f69405adab22fa957f35a8f14d17
dzy <dzy@163.com>
2015-09-06 00:20:54 +0800

    通过drawInRect方法修改占位文字

--- BS/BS/Classes/FriendTrends-关注/View/DZYLoginRegisterTextField.m
+++ BS/BS/Classes/FriendTrends-关注/View/DZYLoginRegisterTextField.m
@@ -18,54 +18,36 @@
     // 文字颜色
     self.textColor = [UIColor whiteColor];
     // 设置带有属性的占位文字(富文本)
-//    self.placeholder;
-    self.attributedPlaceholder = [[NSAttributedString alloc] initWithString:self.placeholder attributes:@{NSForegroundColorAttributeName:[UIColor grayColor]}];
-//    DZYLog(@"%@", self.placeholder);
-    
-//    [self uses];
+//    self.attributedPlaceholder = [[NSAttributedString alloc] initWithString:self.placeholder attributes:@{NSForegroundColorAttributeName:[UIColor grayColor]}];
+
 }
 
-- (void)uses
+// 重写这个方法
+- (void)drawPlaceholderInRect:(CGRect)rect
 {
-    // 富文本用法1-不可变的属性文字
-//    NSMutableDictionary *attrs = [NSMutableDictionary  dictionary];
-//    attrs[NSForegroundColorAttributeName] = [UIColor grayColor];
-//    attrs[NSUnderlineStyleAttributeName] = @1;
-//    attrs[NSUnderlineColorAttributeName] = [UIColor redColor];
+//    DZYLog(@"%@", NSStringFromCGRect(self.bounds));
+    // 占位文字画在那个矩形框里
+    CGRect placeholderRect = self.bounds;
+    placeholderRect.origin.y = (self.height - self.font.lineHeight) * 0.5;
+    
+    NSMutableDictionary *attrs = [NSMutableDictionary dictionary];
+    
+    attrs[NSForegroundColorAttributeName] = [UIColor redColor];
+    attrs[NSFontAttributeName] = self.font;
+    
+    [self.placeholder drawInRect:placeholderRect withAttributes:attrs];
+    
+    // 占位文字画在什么位置
+//    CGPoint point;
+//    point.x = 0;
+//    point.y = (self.height - self.font.lineHeight) * 0.5;
 //    
-//    self.attributedPlaceholder = [[NSAttributedString alloc] initWithString:self.placeholder attributes:attrs];
-    
-    // 富文本用法2-可变的属性文字 红 绿
-//    NSMutableAttributedString *string = [[NSMutableAttributedString alloc] initWithString:self.placeholder];
-//    [string addAttribute:NSForegroundColorAttributeName value:[UIColor redColor] range:NSMakeRange(0, 1)];
-//    [string addAttribute:NSForegroundColorAttributeName value:[UIColor greenColor] range:NSMakeRange(1, 1)];
-//    [string addAttribute:NSFontAttributeName value:[UIFont boldSystemFontOfSize:30] range:NSMakeRange(1, 1)];
-//    self.attributedPlaceholder = string;
-    
-   // 富文本用法3-图文混排 得分别包装3个AttributedString 再调用NSMutableAttributedString 的append方法
-//    NSMutableAttributedString *string = [[NSMutableAttributedString alloc] init];
-//    // 第一段 placeholder
-//    NSAttributedString *subString1 = [[NSAttributedString alloc] initWithString:self.placeholder];
-//    [string appendAttributedString:subString1];
+//    NSMutableDictionary *attrs = [NSMutableDictionary dictionary];
 //    
-//    // 第二段 图片 attachment附件
-//    NSTextAttachment *attachment = [[NSTextAttachment alloc] init];
-//    attachment.image = [UIImage imageNamed:@"login_close_icon"];
-//    attachment.bounds = CGRectMake(0, 0, 16, 16);// x y能调整往上下左右走
-//    
-//    NSAttributedString *subString2 = [NSAttributedString attributedStringWithAttachment:attachment];
-//    [string appendAttributedString:subString2];
-//    
-//    // 第三段 哈哈
-//    NSAttributedString *subString3 = [[NSAttributedString alloc] initWithString:@"哈哈"];
-//    [string appendAttributedString:subString3];
-//    
-//    self.attributedPlaceholder = string;
-    
-    
-    
-    
+//    attrs[NSForegroundColorAttributeName] = [UIColor redColor];
+//    attrs[NSFontAttributeName] = self.font;
+//
+//    [self.placeholder drawAtPoint:point withAttributes:attrs];
     
 }
-
 @end
