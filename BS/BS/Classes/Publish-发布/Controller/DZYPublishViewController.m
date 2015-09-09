d03ce1e888b24e940e0d84db47bc0622d964d794
dzy <dzy@163.com>
2015-09-09 23:49:55 +0800

    退出动画

--- BS/BS/Classes/Publish-发布/Controller/DZYPublishViewController.m
+++ BS/BS/Classes/Publish-发布/Controller/DZYPublishViewController.m
@@ -10,11 +10,14 @@
 #import "DZYPublishButton.h"
 #import <POP.h>
 
-
+static CGFloat const DZYSpringFactor = 10;
 @interface DZYPublishViewController ()
 
 /** 标语 */
 @property (nonatomic, weak) UIImageView *sloganView;
+
+/** 按钮 */
+@property (nonatomic, strong) NSMutableArray *buttons;
 
 /** 动画时间 */
 @property (nonatomic, strong) NSArray *times;
@@ -22,6 +25,14 @@
 @end
 
 @implementation DZYPublishViewController
+
+- (NSMutableArray *)buttons
+{
+    if (!_buttons) {
+        _buttons = [NSMutableArray array];
+    }
+    return _buttons;
+}
 
 - (NSArray *)times
 {
@@ -33,7 +44,7 @@
                    @(2 * interval),
                    @(0 * interval),
                    @(1 * interval),
-                   @(6 * interval)];
+                   @(6 * interval)];// 标语的动画时间
         
     }
     return _times;
@@ -42,6 +53,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    // 禁止交互
+    self.view.userInteractionEnabled = NO;
     
     [self setupSloganView];
     
@@ -69,6 +82,7 @@
         DZYPublishButton *button = [DZYPublishButton buttonWithType:UIButtonTypeCustom];
         button.width = - 1; // 按钮的尺寸为0 还是能看见文字所称一个点 设置按钮的尺寸为负数 那么就看不见文字了
         [button addTarget:self action:@selector(buttonClick:) forControlEvents:UIControlEventTouchUpInside];
+        [self.buttons addObject:button];
         [self.view addSubview:button];
         
         // 设置内容
@@ -84,8 +98,8 @@
         POPSpringAnimation *anim = [POPSpringAnimation animationWithPropertyNamed:kPOPViewFrame];
         anim.fromValue = [NSValue valueWithCGRect:CGRectMake(buttonX, buttonY - DZYScreenH, buttonW, buttonH)];
         anim.toValue = [NSValue valueWithCGRect:CGRectMake(buttonX, buttonY, buttonW, buttonH)];
-        anim.springSpeed = 10;
-        anim.springBounciness = 10;
+        anim.springSpeed = DZYSpringFactor;
+        anim.springBounciness = DZYSpringFactor;
         // CACurrentMediaTime() 获得的是当前的时间
         anim.beginTime = CACurrentMediaTime() + [self.times[i] doubleValue];
         [button pop_addAnimation:anim forKey:nil];
@@ -106,34 +120,56 @@
     self.sloganView = sloganView;
     
     // 动画
+    DZYWeakSelf;
     POPSpringAnimation *anim = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPositionY];
     anim.toValue = @(sloganY);
-    anim.springSpeed = 10;
-    anim.springBounciness = 10;
+    anim.springSpeed = DZYSpringFactor;
+    anim.springBounciness = DZYSpringFactor;
     anim.beginTime = CACurrentMediaTime() + [self.times.lastObject doubleValue];
+    [anim setCompletionBlock:^(POPAnimation *anim, BOOL finished) {
+        // 开始交互
+        weakSelf.view.userInteractionEnabled = YES;
+    }];
     [sloganView.layer pop_addAnimation:anim forKey:nil];
     
 
 }
-- (void)buttonClick:(UIButton *)button
+- (void)buttonClick:(DZYPublishButton *)button
 {
     DZYLogFunc;
 }
+- (IBAction)cancel {
+    
+    // 禁止交互
+    self.view.userInteractionEnabled = NO;
+    
+    // 让按钮执行动画
+    for (int i = 0; i < self.buttons.count; i++) {
+        DZYPublishButton *button = self.buttons[i];
+        
+        POPBasicAnimation *anim = [POPBasicAnimation animationWithPropertyNamed:kPOPLayerPositionY];
+        anim.toValue = @(button.layer.position.y + DZYScreenH);
+        // CACurrentMediaTime()获得的是当前时间
+        anim.beginTime = CACurrentMediaTime() + [self.times[i] doubleValue];
+        [button.layer pop_addAnimation:anim forKey:nil];
+    }
+    
+    DZYWeakSelf;
+    // 让标题执行动画
+    POPBasicAnimation *anim = [POPBasicAnimation animationWithPropertyNamed:kPOPLayerPositionY];
+    anim.toValue = @(self.sloganView.layer.position.y + DZYScreenH);
+    // CACurrentMediaTime()获得的是当前时间
+    anim.beginTime = CACurrentMediaTime() + [self.times.lastObject doubleValue];
+    [anim setCompletionBlock:^(POPAnimation *anim, BOOL finished) {
+        [weakSelf dismissViewControllerAnimated:NO completion:nil];
+    }];
+    [self.sloganView.layer pop_addAnimation:anim forKey:nil];
+}
 
-//        UISwitch *s = [[UISwitch alloc] init];
-//        s.on = YES;
-//        [self.view addSubview:s];
-//
-//        POPBasicAnimation *anim = [POPBasicAnimation animationWithPropertyNamed:kPOPViewCenter];
-//        anim.fromValue = [NSValue valueWithCGPoint:CGPointZero];
-//        anim.toValue = [NSValue valueWithCGPoint:CGPointMake(200, 200)];
-//        anim.duration = 1.0;
-//        [s pop_addAnimation:anim forKey:nil];
-//
-//        POPSpringAnimation *anim = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPosition];
-//        anim.fromValue = [NSValue valueWithCGPoint:CGPointZero];
-//        anim.toValue = [NSValue valueWithCGPoint:CGPointMake(200, 200)];
-//        anim.springSpeed = 10;
-//        anim.springBounciness = 10;
-//        [s.layer pop_addAnimation:anim forKey:nil];
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [self cancel];
+}
+
+
 @end
